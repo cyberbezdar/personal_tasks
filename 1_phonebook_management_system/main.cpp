9e002f89d9eff6fe6b7bac06d55506d17c3b888e
cyberbezdar <liliya.panova@mail.ru>
2021-05-01 02:14:36 +0300

    1_PMS: added main menu of the application
    Added only output of possible application functions

--- 1_phonebook_management_system/main.cpp
+++ 1_phonebook_management_system/main.cpp
@@ -1,4 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+void mainMenuPrint()
+{
+	cout << "---------Main Menu---------\n";
+	cout << "Please, enter your choice:\n";
+	cout << "1 - add new phone record;\n";
+	cout << "2 - show existing phone records;\n";
+	cout << "3 - search a particular phone record;\n";
+	cout << "4 - delete phone records;\n";
+	cout << "5 - exit.\n";
+}
+
+void mainMenu()
+{
+	unsigned short status;
+	while (status != 5)
+	{
+		mainMenuPrint();
+		cin >> status;
+
+		switch (status)
+		{
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				cout << "Program closed\n";
+				break;
+			default:
+				cout << "Please enter correct choice\n";
+				break;
+		}
+	}
+}
+
 int main()
 {
+	mainMenu();
 	return 0;
 }
