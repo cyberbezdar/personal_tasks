a24ac9851807accc7d987e997932c9ac6a1b51c3
cyberbezdar <liliya.panova@mail.ru>
2022-05-10 18:11:03 +0300

    1_PMS: added functionality to add a records to the database

--- 1_phonebook_management_system/functional.cpp
+++ 1_phonebook_management_system/functional.cpp
@@ -1,1 +1,29 @@
+#define DATABASE_PATH "database"
+
 #include "functional.h"
+
+using namespace std;
+
+struct PhoneRecord
+{
+	string phoneNumber;
+	string contactName;
+
+	string getPhoneRecord()
+	{ return phoneNumber + " " + contactName; }
+};
+
+void addNewPhoneRecord()
+{
+	PhoneRecord phoneRecord;
+
+	cout << "Enter phone number: ";
+	cin >> phoneRecord.phoneNumber;
+	cout << "Enter contact name: ";
+	cin >> phoneRecord.contactName;
+
+	ofstream databaseFile(DATABASE_PATH);
+
+	databaseFile << phoneRecord.getPhoneRecord() << "\n";
+	databaseFile.close();
+}
