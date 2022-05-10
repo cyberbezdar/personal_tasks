a5a5878161d5d3434905bfc0257127cef5a6c06f
cyberbezdar <liliya.panova@mail.ru>
2022-05-10 18:09:26 +0300

    1_PMS: added files...
    ...to separate the functional part of the application from the display part

---
+++ 1_phonebook_management_system/functional.h
@@ -0,0 +1,10 @@
+#ifndef PHONEBOOK_MANAGEMENT_SYSTEM_FUNCTIONAL_H
+#define PHONEBOOK_MANAGEMENT_SYSTEM_FUNCTIONAL_H
+phonebook_management_system
+
+#include <iostream>
+#include <fstream>
+
+#include "functional.cpp"
+
+#endif //PERSONAL_TASKS_FUNCTIONAL_H
